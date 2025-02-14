7b26097929ae5e78c87c6d0603f42957b79bef2f
carbofish <carbofish@hotmail.com>
2025-02-14 23:58:01 +0800

    ✨ feat: 致谢 & 版本信息

---
+++ components/VersionView.tsx
@@ -0,0 +1,118 @@
+"use client";
+
+import { Link } from "lucide-react"
+import Image from "next/image"
+
+import { A1CTF_NAME, A1CTF_VERSION } from "@/version";
+
+export function VersionView() {
+    return (
+        <div className="w-full h-full flex items-center justify-center select-none">
+            <div className="flex flex-col items-center">
+                <div className="flex items-center justify-center pt-1 pb-1 pl-4 pr-4 gap-6 mb-10">
+                    <Image
+                        className="dark:invert"
+                        src="/images/A1natas.svg"
+                        alt="A1natas"
+                        width={70}
+                        height={70}
+                        priority
+                    />
+                    <div className="flex flex-col">
+                        <span className="text-2xl font-bold">{ A1CTF_NAME }</span>
+                        <span className="text-xl mt-[-8px] text-orange-500">{ A1CTF_VERSION }</span>
+                    </div>
+                </div>
+                <div className="flex gap-4 items-center">
+                    <span className="text-lg">Developer: </span>
+                    <div className="flex gap-1 items-center pt-1 pb-1 pl-3 pr-3 hover:bg-foreground/10 rounded-md transiiton-[background] duration-300">
+                        <Link size={20} />
+                        <span className="text-lg"
+                            onClick={() => {
+                                window.open('https://github.com/carbofish')
+                            }}
+                        >carbofish</span>
+                    </div>
+                </div>
+                <span className="text-lg mt-5 text-red-400">💖Thanks</span>
+                <div className="flex gap-2 mt-1">
+                    <div className="flex gap-4 items-center">
+                        <div className="flex gap-2 items-center pt-2 pb-2 pl-3 pr-3 hover:bg-foreground/10 rounded-md transiiton-[background] duration-300">
+                            <Link size={20} />
+                            <span className="text-md" 
+                                onClick={() => {
+                                    window.open('https://github.com/Reverier-Xu')
+                                }}
+                            >R2S & Reverier-Xu</span>
+                        </div>
+                        <div className="flex gap-2 items-center pt-2 pb-2 pl-3 pr-3 hover:bg-foreground/10 rounded-md transiiton-[background] duration-300">
+                            <Link size={20} />
+                            <span className="text-md" 
+                                onClick={() => {
+                                    window.open('https://github.com/GZTimeWalker/GZCTF')
+                                }}
+                            >GZCTF & GZTime</span>
+                        </div>
+                    </div>
+                </div>
+                <span className="text-lg mt-5 text-blue-500">⚙️Technologies</span>
+                <div className="flex gap-2 mt-1">
+                    <div className="flex gap-4 items-center">
+                        <div className="flex gap-2 items-center pt-2 pb-2 pl-3 pr-3 hover:bg-foreground/10 rounded-md transiiton-[background] duration-300">
+                            <Link size={20} />
+                            <span className="text-md" 
+                                onClick={() => {
+                                    window.open('https://github.com/facebook/react')
+                                }}
+                            >React</span>
+                        </div>
+                        <div className="flex gap-2 items-center pt-2 pb-2 pl-3 pr-3 hover:bg-foreground/10 rounded-md transiiton-[background] duration-300">
+                            <Link size={20} />
+                            <span className="text-md" 
+                                onClick={() => {
+                                    window.open('https://github.com/shadcn-ui/ui')
+                                }}
+                            >Shadcn UI</span>
+                        </div>
+                        <div className="flex gap-2 items-center pt-2 pb-2 pl-3 pr-3 hover:bg-foreground/10 rounded-md transiiton-[background] duration-300">
+                            <Link size={20} />
+                            <span className="text-md" 
+                                onClick={() => {
+                                    window.open('https://github.com/vercel/next.js')
+                                }}
+                            >Next.js</span>
+                        </div>
+                        <div className="flex gap-2 items-center pt-2 pb-2 pl-3 pr-3 hover:bg-foreground/10 rounded-md transiiton-[background] duration-300">
+                            <Link size={20} />
+                            <span className="text-md" 
+                                onClick={() => {
+                                    window.open('https://github.com/lucide-icons/lucide')
+                                }}
+                            >Lucide icons</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex gap-2 mt-1">
+                    <div className="flex gap-4 items-center">
+                        <div className="flex gap-2 items-center pt-2 pb-2 pl-3 pr-3 hover:bg-foreground/10 rounded-md transiiton-[background] duration-300">
+                            <Link size={20} />
+                            <span className="text-md" 
+                                onClick={() => {
+                                    window.open('https://www.typescriptlang.org/')
+                                }}
+                            >TypeScript</span>
+                        </div>
+                        <div className="flex gap-2 items-center pt-2 pb-2 pl-3 pr-3 hover:bg-foreground/10 rounded-md transiiton-[background] duration-300">
+                            <Link size={20} />
+                            <span className="text-md" 
+                            >More...</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex mt-8">
+                    <span className="text-md font-bold">这是一个处于开发阶段的萌新项目, 有问题请多多指正 :)</span>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ version.ts
@@ -0,0 +1,2 @@
+export const A1CTF_VERSION = "dev-e0579ca"
+export const A1CTF_NAME = "A1CTF Preview"
