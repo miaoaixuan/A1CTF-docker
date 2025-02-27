f926dde8d54e12c5eb4557f37923c0f10756163e
carbofish <carbofish@hotmail.com>
2025-02-27 21:39:51 +0800

    ✨ fix: 修复静态靶机没法开启的问题

--- version.ts
+++ version.ts
@@ -1,3 +1,3 @@
-export const A1CTF_VERSION = "dev-2ad23f0"
+export const A1CTF_VERSION = "dev-cf2daf3"
 export const A1CTF_NAME = "A1CTF Preview"
-export const BUILD_TIME = "2025-02-27T19:53:40.000Z"
+export const BUILD_TIME = "2025-02-27T19:55:20.000Z"
