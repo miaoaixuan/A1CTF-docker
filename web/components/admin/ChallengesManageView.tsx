2ce7936bfece678eb3ed6d3823e877b49620ac2f
carbofish <carbofish@hotmail.com>
2025-03-12 19:51:56 +0800

    ✨ feat: 后台题目列表

--- web/components/admin/ChallengesManageView.tsx
+++ web/components/admin/ChallengesManageView.tsx
@@ -2,10 +2,14 @@
 
 import { MacScrollbar } from "mac-scrollbar";
 import { Button } from "../ui/button";
-import { CirclePlus, Volleyball } from "lucide-react";
+import { CirclePlus, GalleryVerticalEnd, Search, Squirrel, Volleyball } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import { BadgeCent, Binary, Bot, Bug, ChevronDown, ChevronUp, Chrome, CircleArrowLeft, Earth, FileSearch, Github, GlobeLock, HardDrive, MessageSquareLock, Radar, Smartphone, SquareCode } from "lucide-react"
+import { useEffect, useState } from "react";
+import { api, ChallengeInfoModel } from "@/utils/GZApi";
+import { Input } from "../ui/input";
+import { Badge } from "../ui/badge";
 
 export function ChallengesManageView() {
 
@@ -27,7 +31,7 @@ export function ChallengesManageView() {
     };
 
     const cateIcon : { [key: string]: any } = {
-        "all": <Volleyball size={23} />,
+        "all": <Squirrel size={23} />,
         "misc": <Radar size={23} />,
         "crypto": <MessageSquareLock size={23} />,
         "pwn": <Bug size={23} />,
@@ -36,34 +40,81 @@ export function ChallengesManageView() {
         "forensics": <FileSearch size={23} />,
         "hardware": <HardDrive size={23} />,
         "mobile": <Smartphone size={23} />,
-        "ppc": <SquareCode size={23} />,
+        "PPC": <SquareCode size={23} />,
         "ai": <Bot size={23} />,
         "pentent": <BadgeCent size={23} />,
         "osint": <Github size={23} />
     };
 
+    const [ curChoicedCategory, setCurChoicedCategory ] = useState("all")
+    const [ challenges, setChallenges ] = useState<ChallengeInfoModel[]>([])
+
+    useEffect(() => {
+        api.edit.editGetGameChallenges(37).then((res) => {
+            setChallenges(res.data)
+        })
+    }, [])
+
+    const [ searchContent, setSearchContent ] = useState("")
+
+    const filtedData = challenges.filter((chl) => {
+        if (searchContent == "") return curChoicedCategory == "all" || chl.category?.toLowerCase() == curChoicedCategory;
+        else return chl.title.toLowerCase().includes(searchContent.toLowerCase()) && (curChoicedCategory == "all" || chl.category?.toLowerCase() == curChoicedCategory)
+    })
+
     return (
         <div className="w-full h-full flex flex-col p-5 lg:p-10">
-            <div className="flex items-center justify-end mb-6 select-none">
-                {/* <span className="font-bold text-3xl">题目列表</span> */}
+            <div className="flex items-center justify-between mb-6 select-none">
+                <div className="flex gap-4 items-center w-[50%]">
+                    {/* <div className="h-[36px] border-[1px] flex items-center justify-center rounded-lg gap-2 px-3">
+                        <span className="font-bold text-nowrap">{ challenges.length } challenges</span>
+                    </div> */}
+                    <div className="h-[36px] border-[1px] flex items-center justify-center rounded-lg gap-2 px-3">
+                        <Search />
+                        <span className="font-bold text-nowrap">Search in { challenges.length } challengs</span>
+                    </div>
+                    <Input value={searchContent} onChange={(e) => setSearchContent(e.target.value)} placeholder="在这里输入就可以搜索题目标题了"></Input>
+                </div>
                 <Button>
                     <CirclePlus />
                     添加题目
                 </Button>
             </div>
-            <div className="w-full h-full flex">
-                <div className="flex flex-col w-[150px] gap-1 select-none">
-                    <span className="font-bold">Categories</span>
+            <div className="w-full h-full flex overflow-hidden gap-2">
+                <div className="flex flex-col w-[150px] gap-1 select-none flex-none">
+                    <span className="font-bold mb-2">Categories</span>
                     { Object.keys(cateIcon).map((cat, index) => (
-                        <div key={index} className="flex items-center gap-2 px-2 pt-[6px] pb-[6px] hover:bg-foreground/10 rounded-lg transition-colors duration-300">
+                        <Button key={index} className={`flex items-center justify-start gap-2 px-2 pt-[6px] pb-[6px] rounded-lg transition-colors duration-300`}
+                            variant={curChoicedCategory === cat ? "default" : "ghost"}
+                            onClick={() => setCurChoicedCategory(cat)}
+                        >
                             { cateIcon[cat] }
                             <span className="text-md">{ cat.substring(0, 1).toUpperCase() + cat.substring(1) }</span>
-                        </div>
+                            <div className="flex-1"/>
+                            <Badge className={`p-1 h-[20px] ${curChoicedCategory === cat ? "invert" : ""}`}>{ challenges.filter((res) => ( cat == "all" || res.category?.toLowerCase() == cat )).length }</Badge>
+                        </Button>
                     )) }
                 </div>
-                <div className="flex flex-col flex-1">
-
-                </div>
+                {/* <div className="flex-1 overflow-hidden"> */}
+                { filtedData.length ? (
+                    <MacScrollbar className="overflow-y-auto flex flex-col gap-4 w-full p-6 pt-2">
+                        {
+                            filtedData.map((chal, index) => (
+                                <div className="w-full h-[100px] flex border-2 shadow-lg rounded-lg p-4 flex-none" key={index}>
+                                    <div className="flex gap-2">
+                                        { cateIcon[chal.category?.toLowerCase() || "misc"] }
+                                        <span>{ chal.title }</span>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </MacScrollbar>
+                ) : (
+                    <div className="flex w-full h-full items-center justify-center">
+                        <span className="font-bold text-xl">No challenges found</span>
+                    </div>
+                ) }
+                {/* </div> */}
             </div>
         </div>
     );
